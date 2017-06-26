c484cbbb8707bf2bafd73ad62735336447b2e328
Ryo Taguchi <ganeme816@gmail.com>
2017-06-26 22:01:24 +0900

    Initial Commit

---
+++ TouhouEncyclopedia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TouhouEncyclopedia
+//
+//  Created by ganeme816 on 2017/06/26.
+//  Copyright © 2017年 RyoTaguchi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
